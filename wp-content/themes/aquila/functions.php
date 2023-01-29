f6aec8c57d399cd0dffd15fdc7862187a2ec79db
Md. Monirul Islam <mmimonir.ctg@gmail.com>
2023-01-30 02:15:58 +0600

    autolod file created

--- wp-content/themes/aquila/functions.php
+++ wp-content/themes/aquila/functions.php
@@ -2,6 +2,18 @@
 // echo '<pre>';
 // print_r(filemtime(get_template_directory() . '/style.css'));
 // wp_die();
+if (!defined('AQUILA_DIR_PATH')) {
+    define('AQUILA_DIR_PATH', untrailingslashit(get_template_directory()));
+}
+
+
+require_once AQUILA_DIR_PATH . '/inc/helpers/autoloader.php';
+
+function aquila_get_theme_instance()
+{
+    \AQUILA_THEME\Inc\AQUILA_THEME::get_instance();
+};
+aquila_get_theme_instance();
 
 function aquila_enqueue_scripts()
 {   // Registering Styles
---
+++ wp-content/themes/aquila/inc/classes/class-aquila-theme.php
@@ -0,0 +1,33 @@
+<?php
+
+/**
+ * Bootstraps the Theme.
+ *
+ * @package Aquila
+ */
+
+namespace AQUILA_THEME\Inc;
+
+use AQUILA_THEME\Inc\Traits\Singleton;
+
+class AQUILA_THEME
+{
+    use Singleton;
+
+    protected function __construct()
+    {
+        // wp_die('Hello World!');
+
+        // Load class.		
+
+        $this->setup_hooks();
+    }
+
+    protected function setup_hooks()
+    {
+
+        /**
+         * Actions.
+         */
+    }
+}
