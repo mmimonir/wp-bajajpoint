9f50dc34b68bb8bbefff0f2fc48d68a49865077b
Md. Monirul Islam <mmimonir.ctg@gmail.com>
2023-02-05 01:35:18 +0600

    add logo support

--- wp-content/themes/aquila/functions.php
+++ wp-content/themes/aquila/functions.php
@@ -1,9 +1,11 @@
 <?php
-// echo '<pre>';
-// print_r(filemtime(get_template_directory() . '/style.css'));
-// wp_die();
+
 if (!defined('AQUILA_DIR_PATH')) {
     define('AQUILA_DIR_PATH', untrailingslashit(get_template_directory()));
+}
+
+if (!defined('AQUILA_DIR_URI')) {
+    define('AQUILA_DIR_URI', untrailingslashit(get_template_directory_uri()));
 }
 
 
@@ -14,22 +16,3 @@ function aquila_get_theme_instance()
     \AQUILA_THEME\Inc\AQUILA_THEME::get_instance();
 };
 aquila_get_theme_instance();
-
-function aquila_enqueue_scripts()
-{   // Registering Styles
-    wp_register_style('style-css', get_stylesheet_uri(), [], filemtime(get_template_directory() . '/style.css'), 'all');
-    wp_register_style('bootstrap-css', get_template_directory_uri() . '/assets/src/library/css/bootstrap.min.css', [], false, 'all');
-
-    // Registering Scripts
-    wp_register_script('main-js', get_template_directory_uri() . '/assets/main.js', [], filemtime(get_template_directory() . '/main.js'), 'all');
-    wp_register_script('bootstrap-js', get_template_directory_uri() . '/assets/src/library/js/bootstrap.min.js', ['jquery'], false, true);
-
-    // Enqueue Styles
-    wp_enqueue_style('style-css');
-    wp_enqueue_style('bootstrap-css');
-
-    // Enqueue Scripts
-    wp_enqueue_script('main-js');
-    wp_enqueue_script('bootstrap-js');
-}
-add_action('wp_enqueue_scripts', 'aquila_enqueue_scripts');
--- wp-content/themes/aquila/inc/classes/class-aquila-theme.php
+++ wp-content/themes/aquila/inc/classes/class-aquila-theme.php
@@ -16,9 +16,8 @@ class AQUILA_THEME
 
     protected function __construct()
     {
-        // wp_die('Hello World!');
-
-        // Load class.		
+        // Load class.
+        Assets::get_instance();
 
         $this->setup_hooks();
     }
@@ -29,5 +28,18 @@ class AQUILA_THEME
         /**
          * Actions.
          */
+        add_action('after_setup_theme', [$this, 'setup_theme']);
+    }
+
+    public function setup_theme()
+    {
+        add_theme_support('title-tag');
+        add_theme_support('custom-logo', [
+            'header-text' => ['site-title', 'site-description'],
+            'height' => 100,
+            'width' => 400,
+            'flex-height' => true,
+            'flex-width' => true,
+        ]);
     }
 }
